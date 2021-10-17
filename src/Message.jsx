340ec1d06c693c69d0d130f2e2fc5424d3168ff1
sergey.makarov <sergey.makarov@maxidom.ru>
2021-10-18 00:43:43 +0300

    Добавил файл стилей для компонента Message

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,9 +1,11 @@
+import "./Message.css";
+
 const Message = (props) => {
-    return (
-        <div>
-            <h1>{props.text}</h1>
-        </div>    
-    );
-}
+  return (
+    <div className="Wrapper">
+      <h1 className="Wrapper__title">{props.text}</h1>
+    </div>
+  );
+};
 
 export default Message;
