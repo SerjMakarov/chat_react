6d973713d44ed981e128d36e4f056a197f82ff9f
sergey.makarov <sergey.makarov@maxidom.ru>
2021-10-29 17:16:05 +0300

    Отправка сообщения ботом с задержкой в 2 сек

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,12 +6,17 @@ import Chat from "./Chat";
 function App() {
   const [messageList, setMessageList] = useState([]);
   const [inputValue, setInputValue] = useState("");
-  const [messageClient, setMessageClient] = useState('');
+  const [messageClient, setMessageClient] = useState("");
   const [btnLock, setBtnLock] = useState(true);
+  const [messageBot] = useState({
+    message: "Сообщение отправлено",
+    author: "bot",
+  });
+  const [send, setSend] = useState(false);
 
   const handleChange = (e) => {
     const message = { message: inputValue, author: "anonym" };
-    setInputValue(message.message = e.target.value);
+    setInputValue((message.message = e.target.value));
     if (inputValue !== "") {
       setMessageClient(message);
       setBtnLock(false);
@@ -23,11 +28,22 @@ function App() {
     setMessageList([...messageList, messageClient]);
     setBtnLock(true);
     setInputValue("");
+    setSend(true);
+  };
+
+  const submitBot = () => {
+    setSend(false);
+    setMessageList([...messageList, messageBot]);
   };
 
   useEffect(() => {
-
-  });
+    const id = setTimeout(() => {
+      if (send) {
+        submitBot();
+      }
+    }, 2000);
+    return () => clearTimeout(id);
+  }, [messageList]);
 
   return (
     <div className="App">
