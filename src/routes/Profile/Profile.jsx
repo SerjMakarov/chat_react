459fa0d6c9cc0552899f849cf527b2e03893c43e
sergey.makarov <sergey.makarov@maxidom.ru>
2021-11-09 12:13:18 +0300

    Добавил хранилище, добавил редюсер

--- src/routes/Profile/Profile.jsx
+++ src/routes/Profile/Profile.jsx
@@ -1,8 +1,23 @@
+import { useSelector, useDispatch } from "react-redux";
+import { TOGGLE_SHOW_PROFILE } from "../../store/profile/action";
+
 export function Profile() {
+  const dispatch = useDispatch();
+  const isShow = useSelector((state) => state.isShow);
 
   return (
-    <div>
-      <h1>Страница профиля</h1>
-    </div>
+      <div>
+        <h1>Страница профиля</h1>
+
+        <input
+          type="checkbox"
+          checked={isShow}
+          onChange={() => {
+            dispatch({
+              type: TOGGLE_SHOW_PROFILE,
+            });
+          }}
+        />
+      </div>
   );
 }
